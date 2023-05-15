cd9e13dd9993673852c964ac8f046c14d597a7dc
Steven Pfeffer <steven.pfeffer@atreo.cz>
2023-05-15 10:37:54 +0000

    fix: directline

--- src/models/enums/app-state.ts
+++ src/models/enums/app-state.ts
@@ -2,7 +2,5 @@ export enum AppState {
     Loading = "loading",
     Loaded = "loaded",
     Popover = "popover",
-    ChatOpen = "chatOpen",
-    ChatClosed = "chatClosed",
     Feedback = "feedback",
 }
